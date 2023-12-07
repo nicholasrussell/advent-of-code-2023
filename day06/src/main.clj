6bcb6b62fa79f3f4e601ca44018f09c297da25cb
nicholasrussell <nick.f.russell@gmail.com>
2023-12-06 19:22:04 -0600

    clean up

--- day06/src/main.clj
+++ day06/src/main.clj
@@ -19,7 +19,7 @@
       (let [sqrt-discr (math/sqrt discriminant)
             inv-2a (/ 1 (* 2 a))
             t0 (* (- (- b) sqrt-discr) inv-2a)
-            t1 (* (+ (- b) (math/sqrt discriminant)) inv-2a)]
+            t1 (* (+ (- b) sqrt-discr) inv-2a)]
         [t0 t1]))))
 
 (defn solve
@@ -28,7 +28,7 @@
        (map
         (fn [race]
           (let [roots (quad-roots -1 (:time race) (- 0 (:record-distance race) 0.00001))]
-            (int (+ (- (math/floor (first roots)) (math/floor (second roots))))))))
+            (int (- (math/floor (first roots)) (math/floor (second roots)))))))
        (reduce * 1)))
 
 ;; Part 1:
@@ -39,9 +39,8 @@
 ;;  27363861
 (defn -main
   [& args]
-  (let [input (file-util/lazy-load-resource "input.txt")
-        data (parse-data input)]
+  (let [input (file-util/lazy-load-resource "input.txt")]
     (println "Part 1:")
-    (println (solve data))
+    (println (solve (parse-data input)))
     (println "Part 2:")
     (println (solve (->> input (map (fn [line] (string/replace line #"\s" ""))) parse-data)))))
